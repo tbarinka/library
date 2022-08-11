4bb6600e5845a3d7219ad962e91e2ed1f029ce31
Taylor Barinka <barinkataylor@gmail.com>
2022-08-11 14:32:12 -0400

    add form whose inputs initiate Book constructor

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const btn = document.querySelector('#store-btn');
+const btn = document.querySelector('#btn');
+const submitBtn = document.querySelector('#submit-btn');
 const container = document.querySelector('#contain');
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
@@ -28,18 +29,9 @@ function loopLibrary() {
 }
 
 //produce div of book
-btn.addEventListener('click', (event) => {
-    
+submitBtn.addEventListener('click', (event) => {
+  let a = author.value;
+  let t = title.value;
+  new Book(a, t);
 });
 
-//button event listener
-//bring up a form with: author, title
-//let form-input call Book function
-//link to addBookToLibrary function
-
-
-btn.addEventListener('click', function() {
-    const div = document.createElement('div');
-    div.textContent = prompt("What is your name?");
-    container.appendChild("div");
-})
