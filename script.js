163586f199f37456566c00d587e4efb8ded46ef0
Taylor Barinka <barinkataylor@gmail.com>
2022-08-12 13:33:29 -0400

    modify addBtn/submitBtn to create divs & append them to libShelf

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const library = document.querySelector('.library-books');
 const form = document.getElementById('form');
+const libShelf = document.getElementById('lib');
 
 let myLibrary = [];
+book1 = new Book("Sam", "Magic Boy");
+book2 = new Book("Tony", "Pizza Party");
 
 //function adds book to myLibrary array
 function addBookToLibrary(book) {
@@ -16,15 +19,22 @@ function addBookToLibrary(book) {
 function Book(author, title) {
   this.author = author
   this.title = title
-  addBookToLibrary(this)
+  addBookToLibrary(this);
 }
 
 //loop through library  
 function loopLibrary() {
   for (let book of myLibrary) {
-    console.log(book.title + ", by " + book.author);
+    const newBook = document.createElement('div');
+    newBook.textContent = book.title + ", " + book.author;
+    libShelf.appendChild(newBook);
+    //create a div
+    //add book info to div
+    //append div to libShelf
+
   }
-}
+};   loopLibrary();
+
 //event listener for button that passes form contents to Book constructor
 submitBtn.addEventListener('click', (event) => {
   let a = author.value;
